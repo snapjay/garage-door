46f1d0f2adfa1369c7d0788070920afe0a6a057d
Dan Shreim <dan@snapjay.com>
2018-11-11 15:22:12 -0500

    Add API back

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 'use strict'
 
+require('dotenv').config()
+const greenLock = require('greenlock-express')
 const path = require('path')
+const api = require('./api')
 
+const bodyParser = require('body-parser')
 const express = require('express')
-const app = express()
-app.use('/', function (req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end('Hello, secure World!\n\n💚 🔒.js')
-})
 
-require('greenlock-express').create({
+const app = express()
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})) // support encoded body
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
+})
+app.use('/api', api)
+app.use('/', express.static(path.join(__dirname, '../public')))
+
+greenLock.create({
   version: 'draft-11',
   server: 'https://acme-v02.api.letsencrypt.org/directory',
   configDir: path.join(__dirname, '../cert'),
@@ -20,47 +31,10 @@ require('greenlock-express').create({
   communityMember: true,
   telemetry: true,
   debug: true
-}).listen(80, 443)
+}).listen(process.env.PORT, process.env.S_PORT)
 
-// require('dotenv').config()
-// const path = require('path')
-
-// const bodyParser = require('body-parser')
-//
-
-// const http = require('http')
-// const server = http.Server(app)
 // const io = require('socket.io')(server)
-//
-// app.use(bodyParser.json()) // support json encoded bodies
-// app.use(bodyParser.urlencoded({extended: true})) // support encoded bodi
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-//   next()
-// })
-// const api = require('./api')
-//
-// app.use('/api', api)
-// app.use('/', express.static(path.join(__dirname, '../public')))
-//
-// require('greenlock-express').create({
-//   version: 'draft-11',
-//   server: 'https://acme-v02.api.letsencrypt.org/directory',
-//   configDir: path.join(__dirname, '../cert'),
-//   email: 'dan@snapjay.com',
-//   approvedDomains: ['api.door.snapjay.com'],
-//   agreeTos: true,
-//   app: require('express')().use('/', function (req, res) {
-//     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-//     res.end('Hello, World!\n\n💚 🔒.js');
-//   }),
-//   communityMember: true,
-//   telemetry: true,
-//   debug: true
-// }).listen(80, 443)
-//
+
 // module.exports = {
 //   server: server,
 //   io: io
