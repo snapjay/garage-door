3051093fbf7920fe43b899cfcd4a05be6db6e730
Dan Shreim <dan@snapjay.com>
2018-11-11 15:22:12 -0500

    Add API back

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 'use strict'
 
 const path = require('path')
+const api = require('./api')
 
+const bodyParser = require('body-parser')
 const express = require('express')
+
 const app = express()
-app.use('/', function (req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end('Hello, secure World!\n\n💚 🔒.js')
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})) // support encoded body
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
 })
+app.use('/api', api)
+app.use('/', express.static(path.join(__dirname, '../public')))
 
 require('greenlock-express').create({
   version: 'draft-11',
@@ -25,41 +34,15 @@ require('greenlock-express').create({
 // require('dotenv').config()
 // const path = require('path')
 
-// const bodyParser = require('body-parser')
 //
 
 // const http = require('http')
 // const server = http.Server(app)
 // const io = require('socket.io')(server)
 //
-// app.use(bodyParser.json()) // support json encoded bodies
-// app.use(bodyParser.urlencoded({extended: true})) // support encoded bodi
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-//   next()
-// })
-// const api = require('./api')
+
 //
-// app.use('/api', api)
-// app.use('/', express.static(path.join(__dirname, '../public')))
-//
-// require('greenlock-express').create({
-//   version: 'draft-11',
-//   server: 'https://acme-v02.api.letsencrypt.org/directory',
-//   configDir: path.join(__dirname, '../cert'),
-//   email: 'dan@snapjay.com',
-//   approvedDomains: ['api.door.snapjay.com'],
-//   agreeTos: true,
-//   app: require('express')().use('/', function (req, res) {
-//     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-//     res.end('Hello, World!\n\n💚 🔒.js');
-//   }),
-//   communityMember: true,
-//   telemetry: true,
-//   debug: true
-// }).listen(80, 443)
+
 //
 // module.exports = {
 //   server: server,
